35a16e0175f6b334a4d903e684855884b59c0ffb
Cameron Newman <cameron@newman.io>
2019-06-18 17:10:47 +1000

    Marking token flag as required

--- internal/pkg/cmd/root.go
+++ internal/pkg/cmd/root.go
@@ -18,13 +18,15 @@ var rootCmd = &cobra.Command{Use: "shell"}
 
 // Execute is the entry point for the MFA command
 func Execute(version string) {
-	rootCmd.PersistentFlags().StringVarP(&awsProfile, "profile", "p", "default", "AWS Profile name in $HOME/.aws/credentials")
-	rootCmd.PersistentFlags().StringVarP(&mfaToken, "token", "t", "", "Current MFA value to use for STS generation")
+    persistentFlags := rootCmd.PersistentFlags()
+    persistentFlags.StringVarP(&awsProfile, "profile", "p", "default", "AWS Profile name in $HOME/.aws/credentials")
+    persistentFlags.StringVarP(&mfaToken, "token", "t", "", "Current MFA value to use for STS generation")
+    cobra.MarkFlagRequired(persistentFlags, "token")
 
-	releaseVersion = version
+    releaseVersion = version
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
